880cc76c61d657b1991deed74238383af2623265
Jonas Gehring <g.jjoe64@googlemail.com>
2011-10-07 06:56:41 +0200

    selectmenu with onChange event

--- src/com/horaz/client/widgets/SelectMenu.java
+++ src/com/horaz/client/widgets/SelectMenu.java
@@ -1,11 +1,33 @@
+/**
+ * This file is part of Horaz.
+ *
+ * Horaz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Horaz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Horaz.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright Joe's App Factory UG (haftungsbeschränkt)
+ */
+
 package com.horaz.client.widgets;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.SelectElement;
+import com.horaz.client.widgets.events.ChangeListener;
+import com.horaz.client.widgets.events.EventFactory;
 
 /**
  * SelectMenu
  * @horaz.htmltag &lt;select&gt;
+ * @horaz.events change
  */
 public class SelectMenu extends BaseWidget<SelectElement> {
 	/**
@@ -39,4 +61,8 @@ public class SelectMenu extends BaseWidget<SelectElement> {
 	private native static void _open(Element elm)/*-{
 		$wnd.jQuery(elm).selectmenu('open');
 	}-*/;
+
+	public void addChangeListener(ChangeListener changeListener) {
+		EventFactory.bindEventHandler(getElement(), "change", changeListener);
+	}
 }
