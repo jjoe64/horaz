f8e3025acc50170ec02cf20ffe58616999f1b0fc
Jonas Gehring <jonas.gehring@inovex.de>
2012-02-29 06:26:26 +0100

    more options for charts

--- src/com/horaz/client/widgets/charting/YaxisOptions.java
+++ src/com/horaz/client/widgets/charting/YaxisOptions.java
@@ -10,22 +10,29 @@ public class YaxisOptions extends BaseOptions {
 	}
 
 	private static native void setTickFormatterNative( JavaScriptObject axisOptions, TickFormatter tickFormatter )
-    /*-{
+	/*-{
 		axisOptions.tickFormatter = function(val, axis) {
 			return tickFormatter.@com.horaz.client.widgets.charting.TickFormatter::formatTickValue(Ljava/lang/Double;)(@java.lang.Double::valueOf(D)(val));
 		};
-    }-*/;
+	}-*/;
 
-    public void setAlignTicksWithAxis(Integer axisNumber) {
+	public void setAlignTicksWithAxis(Integer axisNumber) {
 		put("alignTicksWithAxis", axisNumber==null?null:new JSONNumber(axisNumber));
+	}
+
+	public void setMin(Integer min) {
+		put("min", min==null?null:new JSONNumber(min));
 	}
 
 	public void setPosition(YaxisPosition pos) {
 		put("position", new JSONString(pos.name().toLowerCase()));
 	}
 
-    public void setTickFormatter(TickFormatter tickFormatter) {
-		setTickFormatterNative( getJavaScriptObject(), tickFormatter );
+	public void setTickDecimals(Integer no) {
+		put("tickDecimals", no==null?null:new JSONNumber(no));
 	}
 
+	public void setTickFormatter(TickFormatter tickFormatter) {
+		setTickFormatterNative( getJavaScriptObject(), tickFormatter );
+	}
 }
